1dee763876c32cda82661142d0c1e2ab9e3193ef
BenICioRODRIGO3452 <beach_05skipper@icloud.com>
2025-09-22 17:45:21 +0100

    Setup new 数据备份恢复 feature

---
+++ data_backup_restore_0923_0045_rmc.js
@@ -0,0 +1,70 @@
+// 代码生成时间: 2025-09-23 00:45:20
+const fs = require('fs').promises;
+const path = require('path');
+const _ = require('lodash');
+
+/**
+ * 数据备份恢复工具
+ * @class DataBackupRestore
+ */
+class DataBackupRestore {
+  constructor(backupPath, restorePath) {
+    this.backupPath = backupPath;
+    this.restorePath = restorePath;
+  }
+
+  /**
+   * 备份数据
+   * @param {Function} callback 回调函数
+   */
+  backupData(callback) {
+    fs.readFile(this.backupPath, 'utf8')
+      .then(data => {
+        return fs.writeFile(this.restorePath, data, 'utf8');
+      })
+      .then(() => {
+        callback(null, 'Backup successful');
+      })
+      .catch(error => {
+        callback(error, null);
+      });
+  }
+
+  /**
+   * 恢复数据
+   * @param {Function} callback 回调函数
+   */
+  restoreData(callback) {
+    fs.readFile(this.restorePath, 'utf8')
+      .then(data => {
+        return fs.writeFile(this.backupPath, data, 'utf8');
+      })
+      .then(() => {
+        callback(null, 'Restore successful');
+      })
+      .catch(error => {
+        callback(error, null);
+      });
+  }
+}
+
+/**
+ * 使用示例
+ */
+const backupRestore = new DataBackupRestore('./backup.txt', './restore.txt');
+
+backupRestore.backupData((error, message) => {
+  if (error) {
+    console.error('Backup error:', error);
+  } else {
+    console.log(message);
+  }
+});
+
+backupRestore.restoreData((error, message) => {
+  if (error) {
+    console.error('Restore error:', error);
+  } else {
+    console.log(message);
+  }
+});
